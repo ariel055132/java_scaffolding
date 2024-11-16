8419048d8da0a846c091384b960da305f78a4418
Adrian Li <ariel055132@gmail.com>
2024-11-16 17:09:59 +0800

    StringUtils.java setup

---
+++ java-scaffolding-common/src/main/java/com/test/common/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.test.common.utils;
+
+/**
+ * String 字符共用函式
+ *
+ * @author Adrian
+ * @date 2024/11/16
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+    public static final String AND = "&";
+}
---
+++ java-scaffolding-common/src/test/java/com/test/common/utils/StringUtilsTest.java
@@ -0,0 +1,4 @@
+package com.test.common.utils;
+
+public class StringUtilsTest {
+}
