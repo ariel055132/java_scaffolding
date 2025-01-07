3de171f98cd64066c82ca08ff18dd3b420efdc5e
Adrian Li <ariel055132@gmail.com>
2025-01-07 22:43:47 +0800

    add desensitized function of Chinese Name, Password, Bank Card No, IPv4, and IPv6 address

--- java-scaffolding-sensitive/src/main/java/DesensitizedUtilHutool.java
+++ java-scaffolding-sensitive/src/main/java/DesensitizedUtilHutool.java
@@ -23,4 +23,68 @@ public class DesensitizedUtilHutool extends DesensitizedUtil {
     public static String carLicense(String carLicense) {
         return DesensitizedUtil.carLicense(carLicense);
     }
+
+    /**
+     * 中國地區的名字脫敏
+     * Desensitization of name in China
+     * 只显示第一个汉字，其他隐藏为2个星号，比如：李**
+     *
+     * @param chineseName String 脫敏前的名字 (Name before desensitization)
+     * @return String 脫敏後的名字 (Desensitized name)
+     */
+    public static String chineseName(String chineseName) {
+        return DesensitizedUtil.chineseName(chineseName);
+    }
+
+    /**
+     * 中國地區的密碼脫敏
+     * Desensitization of password in China
+     * 全部隐藏为星号，比如：******
+     *
+     * @param password
+     * @return
+     */
+    public static String password(String password) {
+        return DesensitizedUtil.password(password);
+    }
+
+    /**
+     * 中國地區的銀行卡號脫敏
+     * Desensitization of bank card number in China
+     * 只展示前4位，后面的位数根据卡号决定展示1-4位，如下所示
+     * "1234 2222 3333 4444 6789 9"    ->   "1234 **** **** **** **** 9"
+     * "1234 2222 3333 4444 6789 91"   ->   "1234 **** **** **** **** 91"
+     * "1234 2222 3333 4444 678"       ->   "1234 **** **** **** 678"
+     * "1234 2222 3333 4444 6789"      ->   "1234 **** **** **** 6789"
+     * @param bankCardNo
+     * @return
+     */
+    public static String bankCard(String bankCardNo) {
+        return DesensitizedUtil.bankCard(bankCardNo);
+    }
+
+    /**
+     * 中國地區的 iPv4 地址脫敏
+     * Desensitization of IPv4 address in China
+     * 只顯示前三位，後面的全部以 * 表示
+     * E.G: 192.0.2.1 -> 192.*.*.*
+     *
+     * @param ipv4Address String 脫敏前的 IPv4 地址 (IPv4 address before desensitization)
+     * @return String 脫敏後的 IPv4 地址 (Desensitized IPv4 address)
+     */
+    public static String ipv4(String ipv4Address) {
+        return DesensitizedUtil.ipv4(ipv4Address);
+    }
+
+    /**
+     * 中國地區的 IPv6 地址脫敏
+     * Desensitization of IPv6 address in China
+     * 脫敏範例: 2001:0db8:86a3:08d3:1319:8a2e:0370:7344 -> 2001:*:*:*:*:*:*:*
+     *
+     * @param ipv6Address
+     * @return
+     */
+    public static String ipv6(String ipv6Address) {
+        return DesensitizedUtil.ipv6(ipv6Address);
+    }
 }
--- java-scaffolding-sensitive/src/test/java/DesensitizedUtilHutoolTest.java
+++ java-scaffolding-sensitive/src/test/java/DesensitizedUtilHutoolTest.java
@@ -1,14 +1,77 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class DesensitizedUtilHutoolTest {
     @Test
+    @DisplayName("中國車牌的脫敏測試")
     void testCarLicense() {
         // Given
         String carLicense = "京A12345";
+        String expectedResult = "京A1***5";
         // When
         String actualResult = DesensitizedUtilHutool.carLicense(carLicense);
         // Then
-        Assertions.assertEquals("京A1***5", actualResult);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("中國名字的脫敏測試")
+    void testChineseName() {
+        // Given
+        String chineseName = "王小明";
+        String expectedResult = "王**";
+        // When
+        String actualResult = DesensitizedUtilHutool.chineseName(chineseName);
+        // Then
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("中國密碼的脫敏測試")
+    void testPassword() {
+        // Given
+        String password = "123456";
+        String expectedResult = "******";
+        // When
+        String actualResult = DesensitizedUtilHutool.password(password);
+        // Then
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("中國銀行卡號的脫敏測試")
+    void testBankCard() {
+        // Given
+        String bankCardNo = "1234 2222 3333 4444 6789 9";
+        String expectedResult = "1234 **** **** **** **** 9";
+        // When
+        String actualResult = DesensitizedUtilHutool.bankCard(bankCardNo);
+        // Then
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("中國 IPv4 地址的脫敏測試")
+    void testIPv4() {
+        // Given
+        String ipv4 = "192.0.2.1";
+        String expectedResult = "192.*.*.*";
+        // When
+        String actualResult = DesensitizedUtilHutool.ipv4(ipv4);
+        // Then
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("中國 IPv6 地址的脫敏測試")
+    void testIPv6() {
+        // Given
+        String ipv6 = "2001:0db8:85a3:0000:0000:8a2e:0370:7334";
+        String expectedResult = "2001:*:*:*:*:*:*:*";
+        // When
+        String actualResult = DesensitizedUtilHutool.ipv6(ipv6);
+        // Then
+        Assertions.assertEquals(expectedResult, actualResult);
     }
 }
