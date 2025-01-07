f6e599752e296e8c77799de5f550aed2afd211d0
Adrian Li <ariel055132@gmail.com>
2025-01-07 17:58:04 +0800

    add some comments

--- java-scaffolding-sensitive/src/main/java/DesensitizedUtilHutool.java
+++ java-scaffolding-sensitive/src/main/java/DesensitizedUtilHutool.java
@@ -2,7 +2,9 @@ import cn.hutool.core.util.DesensitizedUtil;
 
 /**
  * 使用 hutools 工具类进行脱敏
- * 如果是用在 CHN 的 project，可考慮使用它進行脱敏
+ * 如果是用在中國地區的 project，可考慮使用它進行脱敏
+ * Use Hutool Utility class for desensitization
+ * If the project is used in China, consider using it for desensitization
  */
 
 public class DesensitizedUtilHutool extends DesensitizedUtil {
@@ -11,7 +13,13 @@ public class DesensitizedUtilHutool extends DesensitizedUtil {
 
     }
 
-
+    /**
+     * 中國地區的車牌脫敏
+     * Desensitization of car license in China
+     *
+     * @param carLicense String 脫敏前的車牌號 (Car license before desensitization)
+     * @return String 脫敏後的車牌號 (Desensitized car license)
+     */
     public static String carLicense(String carLicense) {
         return DesensitizedUtil.carLicense(carLicense);
     }
