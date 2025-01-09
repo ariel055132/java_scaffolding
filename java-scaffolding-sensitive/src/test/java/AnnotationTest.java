06418a311905b51e98f5c8fd5fd0c0230acb7412
Adrian Li <ariel055132@gmail.com>
2025-01-09 20:12:11 +0800

    add test case

--- java-scaffolding-sensitive/src/test/java/AnnotationTest.java
+++ java-scaffolding-sensitive/src/test/java/AnnotationTest.java
@@ -10,6 +10,14 @@ public class AnnotationTest {
         // Given
         DesensitizedAnnotationTest test = new DesensitizedAnnotationTest();
         test.setEnglishName("John Doe");
+        test.setChineseName("王小明");
+        test.setBirthDate("1990-01-01");
+        test.setIdNumber("A123456789");
+        test.setPhoneNumber("0212345678");
+        test.setCellPhoneNumber("0912345678");
+        test.setChineseAddress("台北市信義區信義路");
+        test.setEnglishAddress("No. 1, Sec. 1, Xinyi Rd., Xinyi Dist., Taipei City");
+        test.setEmail("xinyi@yahoo.com");
         // Serialize to JSON
         ObjectMapper objectMapper = new ObjectMapper();
         String jsonResult = objectMapper.writeValueAsString(test);
--- java-scaffolding-sensitive/src/test/java/DesensitizedAnnotationTest.java
+++ java-scaffolding-sensitive/src/test/java/DesensitizedAnnotationTest.java
@@ -3,6 +3,10 @@ import com.test.common.sensitive.enums.DesensitizeType;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 脫敏注解測試用 Entity
+ */
+
 @Getter
 @Setter
 public class DesensitizedAnnotationTest {
@@ -11,5 +15,26 @@ public class DesensitizedAnnotationTest {
 
     @Desensitize(type = DesensitizeType.ENGLISH_NAME)
     private String englishName;
+
+    @Desensitize(type = DesensitizeType.BIRTH_DATE)
+    private String birthDate;
+
+    @Desensitize(type = DesensitizeType.ID_NUMBER)
+    private String idNumber;
+
+    @Desensitize(type = DesensitizeType.PHONE_NUMBER)
+    private String phoneNumber;
+
+    @Desensitize(type = DesensitizeType.CELL_PHONE_NUMBER)
+    private String cellPhoneNumber;
+
+    @Desensitize(type = DesensitizeType.CHINESE_ADDRESS)
+    private String chineseAddress;
+
+    @Desensitize(type = DesensitizeType.ENGLISH_ADDRESS)
+    private String englishAddress;
+
+    @Desensitize(type = DesensitizeType.EMAIL)
+    private String email;
 }
 
