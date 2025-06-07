542c3316e9c9e673449e89c8988442fa46785474
Adrian Li <ariel055132@gmail.com>
2025-06-07 18:13:56 +0800

    add AnotherController for additional API endpoint and documentation

---
+++ java-scaffolding-test/src/main/java/org/example/controller/AnotherController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/another")
+@Tag(name = "Another", description = "Another API")
+public class AnotherController {
+    @GetMapping("/get")
+    public String getAnother() {
+        return "Another details";
+    }
+}
